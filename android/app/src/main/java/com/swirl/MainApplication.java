1723ca96a798528894ea88592db9771875ac0d74
martin <am.azad66@gmail.com>
2018-10-05 22:05:56 +0330

    load image from camera and gallery

--- android/app/src/main/java/com/swirl/MainApplication.java
+++ android/app/src/main/java/com/swirl/MainApplication.java
@@ -3,6 +3,7 @@ package com.swirl;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.wix.pagedcontacts.PagedContactsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new PagedContactsPackage(),
             new VectorIconsPackage(),
             new ReactNativeOneSignalPackage()
--- src/components/MessagePopup/MessagePopup.js
+++ src/components/MessagePopup/MessagePopup.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
-import {Image, KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Image, KeyboardAvoidingView, Text, TextInput, TouchableOpacity, View, Dimensions} from "react-native";
+const {height, width} = Dimensions.get('window');
 import styles from "./style";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import ImagePicker from "react-native-image-picker";
 import close from "../../assets/images/icons/close_red.png";
 import chat from "../../assets/images/icons/chat.png";
 import chatDisable from "../../assets/images/icons/chatDisable.png";
@@ -11,6 +13,14 @@ import next from "../../assets/images/icons/next.png";
 import {SafeAreaView} from "react-navigation";
 import {CONFIG} from "../../../config";
 const colors = CONFIG.colors;
+const options = {
+    title: 'Select Avatar',
+    // customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
 
 export default class MessagePopup extends Component {
     state = {
@@ -23,11 +33,46 @@ export default class MessagePopup extends Component {
 
 
     tabSelectedFunction= (tabSelected)=>{
-        this.setState({tabSelected})
+        this.setState({ tabSelected, avatarSource: '' });
+
+        if( tabSelected === 'chat' ) {
+
+        } else {
+            /**
+             * The first arg is the options object for customization (it can also be null or omitted for default options),
+             * The second arg is the callback which sends object: response (more info in the API Reference)
+             */
+
+            ImagePicker.showImagePicker(options, ( response ) => {
+                console.log('Response = ', response);
+
+                if( response.didCancel ) {
+                    console.log('User cancelled image picker');
+                } else
+                    if( response.error ) {
+                        console.log('ImagePicker Error: ', response.error);
+                    } else
+                        if( response.customButton ) {
+                            console.log('User tapped custom button: ', response.customButton);
+                        } else {
+                            const source = { uri: response.uri };
+
+                            // You can also display the image using data:
+                            // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                            this.setState({
+                                avatarSource: source,
+                            });
+                        }
+            });
+
+        }
+
     };
     render() {
 
-        const { message, tabSelected } = this.state;
+        const { message, tabSelected, avatarSource } = this.state;
+        console.log("this.state.avatarSource", this.state.avatarSource)
         const count = message.length;
         return (
             <SafeAreaView style={styles.container}>
@@ -49,23 +94,35 @@ export default class MessagePopup extends Component {
                         <View></View>
                     </View>
                     <View style={styles.textInputBox}>
+
                         <View>
-                            <TextInput
-                                style={styles.textInput}
-                                placeholderTextColor="#000"
-                                autoCorrect={false}
-                                multiline={true}
-                                value={message}
-                                autoFocus={true}
-                                blurOnSubmit={true}
-                                returnKeyType="next"
-                                ref={( ref ) => {
-                                    this.myTextInput = ref
-                                }}
-                                onChangeText={message => {
-                                    this.setState({ message: message.slice(0, 120) })
-                                }}
-                            />
+                            {
+                                tabSelected === 'chat' &&
+                                <TextInput
+                                    style={styles.textInput}
+                                    placeholderTextColor="#000"
+                                    autoCorrect={false}
+                                    multiline={true}
+                                    value={message}
+                                    autoFocus={true}
+                                    blurOnSubmit={true}
+                                    returnKeyType="next"
+                                    ref={( ref ) => {
+                                        this.myTextInput = ref
+                                    }}
+                                    onChangeText={message => {
+                                        this.setState({ message: message.slice(0, 120) })
+                                    }}
+                                />
+                            }
+                            {
+                                avatarSource &&
+                                <View style={{ width: width-50, maxHeight:height-100,height:height-200, }}>
+                                    <Image resizeMode={'contain'} source={avatarSource} style={styles.actionBoxIcon}/>
+                                </View>
+                            }
+
+
                         </View>
                         <View style={styles.footer}>
                             <View style={styles.footerCounter}>
