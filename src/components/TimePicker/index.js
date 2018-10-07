11adbe347d72d53d7cf3d9dfc73a45559f511c90
martin <am.azad66@gmail.com>
2018-10-07 23:13:06 +0330

    fix bug

--- src/components/TimePicker/index.js
+++ src/components/TimePicker/index.js
@@ -1,18 +1,2 @@
-import Avatar from "./Avatar";
-import { connect } from "react-redux";
-import { withNavigation } from "react-navigation";
-// import { homeSetCloseModal } from "../../store/home";
-// import { profileSetCloseModal } from "../../store/profile";
-
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = dispatch => ({
-	// homeSetCloseModal: data => dispatch(homeSetCloseModal(data)),
-	// profileSetCloseModal: data => dispatch(profileSetCloseModal(data))
-});
-export default withNavigation(
-	connect(
-		mapStateToProps,
-		mapDispatchToProps
-	)(Avatar)
-);
+import TimePicker from "./TimePicker";
+export default TimePicker();
